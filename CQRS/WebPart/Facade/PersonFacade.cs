c7a491b9357e4f476dc16bf50b9693b44c664f72
mojamcpds <mojamcpds@gmail.com>
2014-01-20 22:41:09 +0600

    Third

--- CQRS/WebPart/Facade/PersonFacade.cs
+++ CQRS/WebPart/Facade/PersonFacade.cs
@@ -23,40 +23,41 @@ namespace WebPart.Facade
         
         public void Save(VMPerson person)
         {
-            //ServiceLocator.CommandBus.Send(new CreatePersonCommand(Guid.NewGuid(), person.LastName, person.FirstName, person.Age));
             _bus.Send(new CreatePersonCommand(Guid.NewGuid(), person.LastName, person.FirstName, person.Age));
         }
 
         public void Update(VMPerson person)
         {
-           // ServiceLocator.CommandBus.Send(new UpdatePersonCommand(person.Id, person.LastName, person.FirstName, person.Age));
             _bus.Send(new UpdatePersonCommand(person.Id, person.LastName, person.FirstName, person.Age));
         }
 
         public VMPerson GetById(Guid id)
         {
-           // var person = ServiceLocator.ReportingRepository.GetById<Person>(id);
             var person = _readmodel.GetById<Person>(id);
             VMPerson personviewmodel = new VMPerson();
-            personviewmodel.Id = person.Id;
-            personviewmodel.LastName = person.LastName;
-            personviewmodel.FirstName = person.FirstName;
-            personviewmodel.Age = person.Age;
+            if (person != null)
+            {
+
+                personviewmodel.Id = person.Id;
+                personviewmodel.LastName = person.LastName;
+                personviewmodel.FirstName = person.FirstName;
+                personviewmodel.Age = person.Age;
+                
+            }
             return personviewmodel;
         }
 
         public void Delete(Guid id)
         {
             var person = this.GetById(id);
-            //ServiceLocator.CommandBus.Send(new DeletePersonCommand(person.Id));
             _bus.Send(new DeletePersonCommand(person.Id));
         }
 
         public IEnumerable<VMPerson> GetAll()
         {
-           // var persons = ServiceLocator.ReportingRepository.GetItems<Person>();
             var persons = _readmodel.GetItems<Person>();
             List<VMPerson> vmList = new List<VMPerson>();
+            
             foreach (Person person in persons)
             {
                 var vm = new VMPerson();
